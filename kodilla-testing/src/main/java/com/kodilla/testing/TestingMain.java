9ffbe500872e55fcd8b99629bc959a00ebbaf5c4
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-01-16 18:00:36 +0100

    dodałem nową klasę Calculator oraz testowanie działania tej klasy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int addResult = Calculator.add(2,2);
+        int subResult = Calculator.subtract( 5,3);
+
+        if(addResult == 4){
+            System.out.println("Test dodawania OK");
+        }else{
+
+            System.out.println("Test dodawania zwraca niepoprawny wynik");
+        }
+
+        if(subResult == 2){
+            System.out.println("Test odejmowania OK");
+        }else{
+
+            System.out.println("Test odejmowania zwraca niepoprawny wynik");
+        }
     }
 }
