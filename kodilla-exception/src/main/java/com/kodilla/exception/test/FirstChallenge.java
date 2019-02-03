90db6098f1e9ae32a33e4c519afb5dc6b72c5577
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-02-03 14:56:23 +0100

    8.2 - Zadanie. Obsługa try-catch-finally dla ArtithmeticException.

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,35 @@
+package com.kodilla.exception.test;
+
+import java.time.LocalDate;
+
+public class FirstChallenge {
+    public double divide(double a, double b) throws ArithmeticException {
+            if (b == 0) {
+                throw new ArithmeticException();
+            }
+            return a / b;
+        }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args    */
+    
+        public static void main (String[]args){
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+    try {
+        double result = firstChallenge.divide(3, 0);
+        System.out.println(result);
+    }catch (ArithmeticException e){
+        System.out.println("Wystapił błąd dzielenia przez ZERO " + e);
+    }finally {
+        LocalDate date = LocalDate.now();
+        System.out.println("Data próby wykonania działania: "+ date);
+    }
+
+
+
+
+    }
+
+}
