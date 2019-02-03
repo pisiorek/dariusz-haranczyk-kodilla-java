be960b066f34ddf364273e34960aecd0528dbb63
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-02-03 14:24:52 +0100

    [8.0 - moduł -nauka. Rozbudowa kodu o try-catch dodano finally do try-catch

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -14,28 +14,29 @@ public class FileReader {
         za odnalezienie klas i plików wewnątrz naszej aplikacji - pozwoli nam to odwołać się do naszego pliku
         z imionami.*/
         ClassLoader classLoader = getClass().getClassLoader();
-
-        /*Jest to wywołanie metody, która wskazuje klasie ClassLoader, gdzie powinna szukać pliku, tu: w katalogu file,
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+        /*getResource() jest to wywołanie metody, która wskazuje klasie ClassLoader, gdzie powinna szukać pliku, tu: w katalogu file,
          w którym umieściliśmy nasz plik names.txt (metoda ta zawsze szuka zasobów wewnątrz katalogu resources).
          Następnie pobieramy tak zwany “uchwyt” do pliku za pomocą metody getFile(), co pozwoli nam przekazać odwołanie
          do tego pliku w następnym kroku.
           Dzięki wywołaniu metody getFile() w poprzednim kroku, mamy już dostęp do pliku z imionami z poziomu kodu
-          - teraz jesteśmy w stanie stworzyć obiekt reprezentujący nasz plik tekstowy za pomocą klasy File, które
+          - teraz jesteśmy w stanie stworzyć obiekt reprezentujący nasz plik tekstowy file za pomocą klasy File, które
           również jest dostępna wewnątrz biblioteki Javy - java.io.*/
-        File file = new File(classLoader.getResource("file/names.txt").getFile());
+        //getPath(), zwraca nam dokładną ścieżkę do naszego pliku names.txt
+        //Path path = Paths.get(file.getPath());  //możemy te linię pominąc i wstawić od razu nizej do metody lines() jako metoda statyczna Paths.get()
 
-        //getPath(), zwróca nam dokładną ścieżkę do naszego pliku names.txt
-        //Path path = Paths.get(file.getPath());
-        Path path = Paths.get("file/name.txt");
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))){     //klasa Files pozwala czytać z pliku tekstowego za pomocą metody lines()
 
-        try {
-            //klasa Files pozwala czytać z pliku tekstowego za pomocą metody lines()
             //wynikiem wywołania lines() jest Stream obiektów typu String
-            Stream<String> fileLines = Files.lines(path);
+            fileLines.forEach(System.out::println); //wyswietla cały powyzszy stream funkcja terminalną
 
         } catch (IOException e){
-            System.out.println("Wystąpił wyjątek");
+            System.out.println("Wystąpił wyjątek: " + e);
+        }finally {
+            System.out.println("Działanie finally - niezależnie od błędu. Wyświetlam się zawsze pomimo ");
+        }
     }
+
         //System.out.println(file.getPath());
-    }
+
 }
