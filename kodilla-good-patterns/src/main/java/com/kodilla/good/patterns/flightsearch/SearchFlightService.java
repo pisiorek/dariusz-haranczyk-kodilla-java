8d33787710d74034ac0252c6a133b19f402447f1
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-02-16 11:03:17 +0100

    9.5 Zadanie - Wyszukiwanie lotów bezpośrednich oraz łączonych, dodano klasę SearchFlightService, a wniej metodę streamProcessor, dzięki temu ograniczono kod w klasie main oraz zredukowano powtarzający się kod.

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flightsearch/SearchFlightService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flightsearch/SearchFlightService.java
@@ -15,28 +15,27 @@ public class SearchFlightService {
         this.flightRetriever = flightRetriever;
     }
 
+    public void streamProccesor( Map<String, Flight> resultFlights){
+        System.out.println("Quantity of flights: " + resultFlights.size());
+        resultFlights.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+        System.out.println("\n");
+    }
+
     public void serchFlight(){
         System.out.println("All flights from airport in: " + startAirport);
     Map<String,Flight> resultFlights = flightRetriever.getFlights().stream()
             .filter(flight -> flight.getDepartureAirport().equals(startAirport))
             .collect(Collectors.toMap(Flight::getNumber, flight -> flight));
-
-        System.out.println("Quantity of flights: " + resultFlights.size());
-        resultFlights.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())
-            .forEach(System.out::println);
-        System.out.println("\n");
+    streamProccesor(resultFlights);
 
         System.out.println("All flights to airport in: " + destinationAirport);
     Map<String,Flight> resultFlights1 = flightRetriever.getFlights().stream()
             .filter(flight -> flight.getArrivalAirport().equals(destinationAirport))
             .collect(Collectors.toMap(Flight::getNumber, flight -> flight));
 
-        System.out.println("Quantity of flights: " + resultFlights1.size());
-        resultFlights1.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())
-            .forEach(System.out::println);
-        System.out.println("\n");
+    streamProccesor(resultFlights1);
 
     Map<String,Flight> resultFlights2 = flightRetriever.getFlights().stream()
             .filter(flight -> flight.getDepartureAirport().equals(startAirport) || flight.getArrivalAirport().equals(destinationAirport))
