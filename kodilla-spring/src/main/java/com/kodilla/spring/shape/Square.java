7f56472f68e815923c0923f2950aa92761d5c529
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-02-18 19:48:36 +0100

    10.2_Dodano przykład gdzi jest wymuszone zastosowanie @Beana i wykluczone @Component

--- kodilla-spring/src/main/java/com/kodilla/spring/shape/Square.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/shape/Square.java
@@ -1,7 +1,8 @@
 package com.kodilla.spring.shape;
 
 public class Square implements Shape {
-    @Override
+
+        @Override
         public String getShapeName() {
             return "This is a square.";
     }
