39bb62d963576b6cee890ff8a866914379103e79
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-02-19 18:44:09 +0100

    10.3_Nauka_LibraryDataBase_przykład z Adnotacje @Qualifier oraz @Conditional

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flightsearch/FlightRetriever.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flightsearch/FlightRetriever.java
@@ -1,9 +1,11 @@
 package com.kodilla.good.patterns.flightsearch;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public final class FlightRetriever {
+/*public final class FlightRetriever {
 
     private  final List<Flight> flights = new ArrayList<Flight>();
 
@@ -29,4 +31,26 @@ public final class FlightRetriever {
     public List<Flight> getFlights() {
         return new ArrayList<>(flights);
     }
+}*/
+
+public final class FlightRetriever {
+
+    private Map<Integer, Flight> flights = new HashMap<Integer, Flight>();
+    //private  final List<Flight> flights = new ArrayList<Flight>();
+
+    public FlightRetriever(){
+
+        flights.put(1, new Flight("Warszawa", "Kraków", "001"));
+        flights.put(2, new Flight("Kraków", "Warszawa", "002"));
+
+        flights.put(3, new Flight("Warszawa", "Gdańsk", "003"));
+        flights.put(4, new Flight("Gdańsk", "Warszawa", "004"));
+
+
+    }
+
+    public Map<Integer, Flight> getFlights() {
+        return new HashMap<>(flights);
+    }
 }
+
