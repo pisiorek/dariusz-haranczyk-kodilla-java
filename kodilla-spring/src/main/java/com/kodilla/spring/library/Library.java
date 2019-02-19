2ea4f9d0a221749d41937dd758cfda16217e503e
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-02-19 11:06:49 +0100

    10.2_Nauka_LibraryDataBase_wstrzykiwanie zależnośic przez metodę - setter - NIEZALECANE ze względu na zasadę zachowanie niemultowalności

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -9,13 +9,10 @@ import java.util.List;
 public final class Library {
     private final List<String> books = new ArrayList<>();
     private LibraryDbController libraryDbController;
-    @Autowired
-    public Library(final LibraryDbController libraryDbController) {
-        this.libraryDbController = libraryDbController;
-    }
 
-    public Library() {
-        // do nothing
+    @Autowired
+    public void setLibraryDbController(LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
     }
 
     public void saveToDb() {
