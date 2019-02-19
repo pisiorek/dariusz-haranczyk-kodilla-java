f0c674168c25100c61f00dee663c902cbf35896b
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-02-19 11:04:54 +0100

    10.2_Nauka_LibraryDataBase_wstrzykiwanie zależnośic przez konstruktor

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.library;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 public final class Library {
     private final List<String> books = new ArrayList<>();
     private LibraryDbController libraryDbController;
-
+    @Autowired
     public Library(final LibraryDbController libraryDbController) {
         this.libraryDbController = libraryDbController;
     }
