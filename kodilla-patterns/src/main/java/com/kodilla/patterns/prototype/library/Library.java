48496389752f9a135f1b7d27af4dc1afa4438c13
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-02-25 12:01:24 +0100

    11.2_Zadanie deep copy, przygotowanie klas pod zadanie

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.prototype.library;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Library extends Prototype {
+    final String name;
+    final Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library) super.clone();
+    }
+}
