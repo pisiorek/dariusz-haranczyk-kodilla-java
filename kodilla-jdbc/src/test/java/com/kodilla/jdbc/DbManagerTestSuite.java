71a6a445dd8bddf0d0b607eba6328683721d59f3
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-03-04 11:17:42 +0100

    12_Nauka_utworzenie TEST połączenia

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -3,7 +3,10 @@ package com.kodilla.jdbc;
 import com.kodilla.jdbc.DbManager;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DbManagerTestSuite {
     @Test
@@ -13,5 +16,34 @@ public class DbManagerTestSuite {
         DbManager dbManager = DbManager.getInstance();
         //Then
         Assert.assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    public void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(6, counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+
+
     }
 }
