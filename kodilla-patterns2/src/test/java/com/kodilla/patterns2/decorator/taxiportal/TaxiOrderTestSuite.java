3c7290490197ab5fac6122a064e3249720f9a422
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-04-28 09:18:42 +0200

    20.2_Nauka - dekorator - implementacja kolejnych funkcjonalności z wykładu

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/taxiportal/TaxiOrderTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/taxiportal/TaxiOrderTestSuite.java
@@ -32,8 +32,8 @@ public class TaxiOrderTestSuite {
     @Test
     public void testTaxiNetworkGetCost(){
         //Given
-        TaxiOrder theOrder = new BasicTaxiOrder();
-        theOrder = new TaxiNetworkOrderDecorator(theOrder);
+        TaxiOrder theOrder = new BasicTaxiOrder();  //tworzymy delegata (obiekt bazowej klasy)
+        theOrder = new TaxiNetworkOrderDecorator(theOrder); //tworzymy dekorator i przekazujemy mu delegata w konstruktorze
 
         //When
         BigDecimal theCost = theOrder.getCost();
@@ -53,5 +53,71 @@ public class TaxiOrderTestSuite {
 
         //Then
         assertEquals("Drive a course by Taxi Network", description);
+    }
+
+    @Test
+    public void testMyTaxiWithChildSeatGetCost(){
+        //Given
+        TaxiOrder taxiOrder = new BasicTaxiOrder();
+        taxiOrder = new MyTaxiNetworkOrderDecorator(taxiOrder);
+        taxiOrder = new ChildSeatDecorator(taxiOrder); // dodajemy fotelik dla dziecka
+        taxiOrder = new ChildSeatDecorator(taxiOrder); // dodajemy kolejny fotelik dla dziecka - kaskada
+
+        //When
+        BigDecimal theCost = taxiOrder.getCost();
+
+        //Then
+        assertEquals(new BigDecimal(39), theCost);
+    }
+
+    @Test
+    public void testMyTaxiWithChildSeatGetDescription(){
+        //Given
+        TaxiOrder taxiOrder = new BasicTaxiOrder();
+        taxiOrder = new MyTaxiNetworkOrderDecorator(taxiOrder);
+        taxiOrder = new ChildSeatDecorator(taxiOrder);
+        taxiOrder = new ChildSeatDecorator(taxiOrder);
+
+        //When
+        String description = taxiOrder.getDescription();
+
+        //Then
+        assertEquals("Drive a course by MyTaxi Network + child seat + child seat", description);
+
+    }
+
+    @Test
+    public void testMyTaxiWithChildSeatExpressVipGetCost(){
+        //Given
+        TaxiOrder taxiOrder = new BasicTaxiOrder();
+        taxiOrder = new MyTaxiNetworkOrderDecorator(taxiOrder);
+        taxiOrder = new ChildSeatDecorator(taxiOrder); // dodajemy fotelik dla dziecka
+        taxiOrder = new ChildSeatDecorator(taxiOrder); // dodajemy kolejny fotelik dla dziecka - kaskada
+        taxiOrder = new ExpressDecorator(taxiOrder); //dodajemy usługę Express
+        taxiOrder = new VipDecorator(taxiOrder); //dodajemy usługę VIP
+
+        //When
+        BigDecimal theCost = taxiOrder.getCost();
+
+        //Then
+        assertEquals(new BigDecimal(54), theCost);
+    }
+
+    @Test
+    public void testMyTaxiWithChildSeatExpressVipGetDescription(){
+        //Given
+        TaxiOrder taxiOrder = new BasicTaxiOrder();
+        taxiOrder = new MyTaxiNetworkOrderDecorator(taxiOrder);
+        taxiOrder = new ChildSeatDecorator(taxiOrder);
+        taxiOrder = new ChildSeatDecorator(taxiOrder);
+        taxiOrder = new ExpressDecorator(taxiOrder); //dodajemy usługę Express
+        taxiOrder = new VipDecorator(taxiOrder); //dodajemy usługę VIP
+
+        //When
+        String description = taxiOrder.getDescription();
+
+        //Then
+        assertEquals("Drive a course by MyTaxi Network + child seat + child seat + express service + variant VIP", description);
+
     }
 }
