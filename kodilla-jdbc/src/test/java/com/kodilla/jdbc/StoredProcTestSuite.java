cfea512098841b598f1365694e888ed74be22923
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-05-10 10:58:56 +0200

    22.3_zadanie dla mentora - dodano testy dla UpdateBooksBestseller

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -12,21 +12,46 @@ public class StoredProcTestSuite {
     public void testUpdateVipLevels() throws SQLException{
         //Given
         DbManager dbManager = DbManager.getInstance();                      //nawiązanie połączenia z bazą
-        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";      //zapytanie ustwiające pola w tabeli READERS
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";      //zapytanie ustwiające pola w tabeli READERS na wartość Not set - potrzebne do testów
         Statement statement = dbManager.getConnection().createStatement();
         statement.executeUpdate(sqlUpdate);                                 //wywołanie update - stsujemy dla aktualizacji danych bezpośredniow bazie
 
         //When
-        String sqlProcedureCall = "CALL UpdateVipLevels()";                 //ustawienie procedury
+        String sqlProcedureCall = "CALL UpdateVipLevels()";                 //ustawienie procedury akyualizujacej - ma wstawić do READERS wartości VIP_Level
         statement.execute(sqlProcedureCall);                                //wywołanie procedury
 
         //Then
-        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\""; //zapytanie zliczające rekordy z Not set (wynik = 0 po wywołaniu VIP_LEVEL)
         ResultSet rs = statement.executeQuery(sqlCheckTable);
         int howMany = -1;
         if(rs.next()){
             howMany = rs.getInt("HOW_MANY");
         }
         assertEquals(0,howMany);
+    }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException{
+        //Given
+        DbManager dbManager = DbManager.getInstance();                      //nawiązanie połączenia z bazą
+        //String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"null\"";      //zapytanie ustwiające pola w tabeli READERS na wartość NULL - potrzebne do testów
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=false";      //zapytanie ustwiające pola w tabeli READERS na wartość NULL - potrzebne do testów
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);                                 //wywołanie update - stsujemy dla aktualizacji danych bezpośredniow bazie
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBooksBestseller()";           //ustawienie procedury aktualizujacej - ma wstawić do BOOKS wartości BESTSELLER zgiodnie z
+        statement.execute(sqlProcedureCall);                                //wywołanie procedury
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=false"; //zapytanie zliczające rekordy z nullem (wynik = 1 po wywołaniu UpdateBooksBestseller)
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = 0;
+        if(rs.next()){
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(1,howMany);
+
+
     }
 }
