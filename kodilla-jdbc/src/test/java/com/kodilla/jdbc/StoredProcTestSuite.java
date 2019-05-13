cd99281f2f7ed5581b639eba5391a070c8f64aee
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-05-13 14:33:42 +0200

    22.3_zadanie dla mentora - dodano testy dla UpdateBooksBestseller - korekty

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -17,11 +17,11 @@ public class StoredProcTestSuite {
         statement.executeUpdate(sqlUpdate);                                 //wywołanie update - stsujemy dla aktualizacji danych bezpośredniow bazie
 
         //When
-        String sqlProcedureCall = "CALL UpdateVipLevels()";                 //ustawienie procedury akyualizujacej - ma wstawić do READERS wartości VIP_Level
+        String sqlProcedureCall = "CALL UpdateVipLevels()";                 //ustawienie procedury aktualizujacej - ma wstawić do READERS wartości VIP_Level
         statement.execute(sqlProcedureCall);                                //wywołanie procedury
 
         //Then
-        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\""; //zapytanie zliczające rekordy z Not set (wynik = 0 po wywołaniu VIP_LEVEL)
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\""; //zapytanie zliczające rekordy z "Not set" (wynik = 0 po wywołaniu VIP_LEVEL)
         ResultSet rs = statement.executeQuery(sqlCheckTable);
         int howMany = -1;
         if(rs.next()){
@@ -36,7 +36,7 @@ public class StoredProcTestSuite {
         DbManager dbManager = DbManager.getInstance();                      //nawiązanie połączenia z bazą
         String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=false";             //zapytanie ustwiające pola w tabeli READERS na wartość NULL - potrzebne do testów
         Statement statement = dbManager.getConnection().createStatement();
-        statement.executeUpdate(sqlUpdate);                                 //wywołanie update - stsujemy dla aktualizacji danych bezpośredniow bazie
+        statement.executeUpdate(sqlUpdate);                                 //wywołanie update - stosujemy dla aktualizacji danych bezpośrednio w bazie
 
         //When
         String sqlProcedureCall = "CALL UpdateBooksBestseller()";           //ustawienie procedury aktualizujacej - ma wstawić do BOOKS wartości BESTSELLER zgiodnie z
